49b9a736fe762b0d150e62cdb9a6ae33f0f8ec39
Vadim <dev.tcaregorodtcev@gmail.com>
2023-06-02 17:32:20 +0300

    add point

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,7 +5,7 @@ from brain_games.common import game, get_db
 
 def main():
     db = get_db('brain_even.json')
-    game(db, 'Answer "yes" if the number is even, otherwise answer "no"')
+    game(db, 'Answer "yes" if the number is even, otherwise answer "no".')
 
 
 if __name__ == "__main__":
